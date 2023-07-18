d1718ac43952a89dbbbc59fbeb0122d36f6d8629
fuentesgBrian <fuentesg.brian@gmail.com>
2023-07-18 01:14:12 -0400

    se modifica el admin.py eliminando los fields que generan un KeyError

--- laboratorio/admin.py
+++ laboratorio/admin.py
@@ -3,19 +3,16 @@ from .models import Laboratorio, DirectorGeneral, Producto
 # Register your models here.
 
 class LaboratorioAdmin(admin.ModelAdmin):
-    fields = ['id', 'nombre']
     list_display = ('id', 'nombre')
     list_display_links = ['nombre']
     ordering = ('id',)
 
 class DirectorGeneralAdmin(admin.ModelAdmin):
-    fields = ['id', 'nombre', 'laboratorio']
     list_display = ('id', 'nombre', 'laboratorio')
     list_display_links = ['nombre', 'laboratorio']
     ordering = ('nombre',)
 
 class ProductoAdmin(admin.ModelAdmin):
-    fields = ['id', 'nombre', 'laboratorio', 'f_fabricacion', 'p_costo', 'p_venta']
     list_display = ('id', 'nombre', 'laboratorio', 'f_fabricacion', 'p_costo', 'p_venta')
     list_display_links = ['nombre', 'laboratorio']
     ordering = ('nombre', 'laboratorio')
